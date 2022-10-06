c9368ecdb07c3ef3c946935abd3b6617353ef746
Arlind-eduongo <arlind@eduongo.com>
2022-10-06 12:04:42 +0200

    changes

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -6,15 +6,48 @@ import es from 'date-fns/locale/es' // the locale you want
 import Event from '../Event'
 import './index.css'
 import moment from 'moment'
+import { random } from 'lodash'
 
 export default function Calendar() {
   const [startDate, setStartDate] = useState(new Date())
   const currentLanguage = useSelector((state) => state.lang.locale)
+  const [eventsForDay, setEventsForDay] = useState([])
   const [selectedDate, setSelectedDate] = useState()
   // eslint-disable-next-line
-  const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([
+    // {
+    //   id: 1,
+    //   title:
+    //     'Live Q&A with Story in Motion Podcast Episode 1 Guest: Adam Marshall',
+    //   author: 'Anastasia Hall',
+    //   date: '2022/10/05',
+    //   time: '12:00 pm'
+    // },
+    // {
+    //   id: 2,
+    //   title:
+    //     'Live Q&A with Story in Motion Podcast Episode 1 Guest: Adam Marshall',
+    //   author: 'Anastasia Hall',
+    //   date: '2022/10/06',
+    //   time: '01:00 pm'
+    // },
+    // {
+    //   id: 2,
+    //   title:
+    //     'Live Q&A with Story in Motion Podcast Episode 1 Guest: Adam Marshall',
+    //   author: 'Anastasia Hall',
+    //   date: '2022/10/06',
+    //   time: '01:00 pm'
+    // }
+  ])
 
   let dates = events.map((data) => new Date(data.date))
+
+  const filterUpComing = (data) => {
+    setEventsForDay(
+      events.filter((event) => moment(data).format('YYYY/MM/DD') == event.date)
+    )
+  }
 
   registerLocale('es', es)
 
@@ -25,13 +58,37 @@ export default function Calendar() {
           <div className='col-md-12 col-12 mx-auto mx-md-auto mx-0 gx-0 gx-md-auto calendar-under-threehoundertpx pe-2'>
             <DatePicker
               locale={currentLanguage}
-              onChange={(date) => setStartDate(date)}
+              onChange={(date) => {
+                setSelectedDate(date ? date : startDate)
+                filterUpComing(date)
+                setStartDate(date)
+              }}
               selected={startDate}
               highlightDates={dates}
               inline
               autoFocus={false}
             />
           </div>
+        </div>
+        <div className='mt-4'>
+          {eventsForDay.length != 0 && (
+            <p className='text-center' style={{ fontWeight: '600' }}>
+              Events for this day
+            </p>
+          )}
+          {eventsForDay.length != 0
+            ? eventsForDay.map((event, index) => (
+                <>
+                  <Event
+                    date={event.date}
+                    time={event.time}
+                    title={event.title}
+                    key={index + 111}
+                    author={event.author}
+                  />
+                </>
+              ))
+            : 'We do not have any event on this date'}
         </div>
         <div className='col-xl-12 col-md-6 col-sm-12'>
           {events.length != 0 && (
@@ -40,24 +97,6 @@ export default function Calendar() {
             </h4>
           )}
           <div>
-            {events.length == 0 && (
-              <div
-                style={{
-                  minHeight: '170px'
-                }}
-                className='d-flex w-100 text-center'
-              >
-                <p
-                  className='mx-auto my-auto no-selected-data'
-                  style={{
-                    fontWeigt: '900px',
-                    fontSize: '16px'
-                  }}
-                >
-                  There not any event in the selected date
-                </p>
-              </div>
-            )}
             <React.Fragment>
               {events.map((event, index) => (
                 <>
