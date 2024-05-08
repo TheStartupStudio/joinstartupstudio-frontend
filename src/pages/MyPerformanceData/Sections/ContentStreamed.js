d1471d5511600a88969e6e096df8ca6def01f87a
bledonibishi <bledonibishi1@gmail.com>
2024-05-08 16:32:14 +0200

    content streamed % fix

--- src/pages/MyPerformanceData/Sections/ContentStreamed.js
+++ src/pages/MyPerformanceData/Sections/ContentStreamed.js
@@ -11,42 +11,46 @@ import CustomSpinner from '../../../components/CustomSpinner'
 
 const DisplayCircleData = ({
   backgroundColor,
-  color,
   title,
   percentage,
   marginTop,
   loading
 }) => {
   return (
-    <div className={'d-flex justify-content-center'} style={{ marginTop }}>
-      <div
-        className={'p-2'}
-        style={{
-          width: 200,
-          height: 200,
-          borderRadius: '50%',
-          backgroundColor: backgroundColor ?? '#54c7df',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          boxShadow:
-            'rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px'
-        }}
-      >
-        <div className={'text-center'} style={{ color: '#fff', fontSize: 20 }}>
-          {title}
-        </div>
-        {loading ? (
-          <CustomSpinner />
-        ) : (
+    <div className={'p-2'}>
+      <div className={'d-flex justify-content-center'} style={{ marginTop }}>
+        <div
+          className={'p-2'}
+          style={{
+            width: 200,
+            height: 200,
+            borderRadius: '50%',
+            backgroundColor: backgroundColor ?? '#54c7df',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            boxShadow:
+              'rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px'
+          }}
+        >
           <div
             className={'text-center'}
-            style={{ color: '#fff', fontWeight: 600, fontSize: 24 }}
+            style={{ color: '#fff', fontSize: 20 }}
           >
-            {percentage}%
+            {title}
           </div>
-        )}
+          {loading ? (
+            <CustomSpinner />
+          ) : (
+            <div
+              className={'text-center'}
+              style={{ color: '#fff', fontWeight: 600, fontSize: 24 }}
+            >
+              {percentage} {typeof percentage === 'string' ? '' : '%'}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
@@ -110,8 +114,7 @@ const ContentStreamed = () => {
                 { label: 'Instructor', value: 'instructor' },
                 { label: 'LTS1', value: 'LTS1' },
                 { label: 'LTS2', value: 'LTS2' },
-                { label: 'LTS3', value: 'LTS3' },
-                { label: 'FinLit', value: 'finlit' }
+                { label: 'LTS3', value: 'LTS3' }
               ]}
               value={filterBy}
               onChange={handleFilterChange}
@@ -119,45 +122,50 @@ const ContentStreamed = () => {
           </div>
           <div className={'row pt-4'}>
             <div className={'col-lg-6 col-md-12'}>
-              <div className={'p-2'}>
-                <DisplayCircleData
-                  backgroundColor={'#54c7df'}
-                  title={'Masterclasses'}
-                  percentage={
-                    !isNaN(masterclassPercentage)
-                      ? masterclassPercentage.toFixed(2)
+              <DisplayCircleData
+                backgroundColor={'#54c7df'}
+                title={'Masterclasses'}
+                percentage={
+                  typeof masterclassPercentage !== 'string'
+                    ? !isNaN(masterclassPercentage)
+                      ? +masterclassPercentage.toFixed(2)
                       : 0
-                  }
-                  loading={myPerformanceData.masterclassLoading}
-                />
-              </div>
+                    : masterclassPercentage
+                }
+                loading={myPerformanceData.masterclassLoading}
+              />
             </div>
+
             <div className={'col-lg-6 col-md-12'}>
-              <div className={'p-2'}>
-                <DisplayCircleData
-                  backgroundColor={'#ff3399'}
-                  title={'Story in Motion Episodes'}
-                  percentage={
-                    !isNaN(podcastPercentage) ? podcastPercentage.toFixed(2) : 0
-                  }
-                  loading={myPerformanceData.podcastLoading}
-                />
-              </div>
+              <DisplayCircleData
+                backgroundColor={'#ff3399'}
+                title={'Story in Motion Episodes'}
+                percentage={
+                  typeof podcastPercentage !== 'string'
+                    ? !isNaN(podcastPercentage)
+                      ? +podcastPercentage.toFixed(2)
+                      : 0
+                    : podcastPercentage
+                }
+                loading={myPerformanceData.podcastLoading}
+              />
             </div>
           </div>
           <div className={'row'}>
             <div className={'col-md-12'} style={{ gap: 20 }}>
-              <div className={'p-2'}>
-                <DisplayCircleData
-                  marginTop={windowWidth < 995 ? '' : '-10%'}
-                  backgroundColor={'#99cc33'}
-                  title={'Q&As'}
-                  percentage={
-                    !isNaN(qaPercentage) ? qaPercentage.toFixed(2) : 0
-                  }
-                  loading={myPerformanceData.qaLoading}
-                />
-              </div>
+              <DisplayCircleData
+                marginTop={windowWidth < 995 ? '' : '-10%'}
+                backgroundColor={'#99cc33'}
+                title={'Q&As'}
+                percentage={
+                  typeof qaPercentage !== 'string'
+                    ? !isNaN(qaPercentage)
+                      ? +qaPercentage.toFixed(2)
+                      : 0
+                    : qaPercentage
+                }
+                loading={myPerformanceData.qaLoading}
+              />
             </div>
           </div>
         </div>
