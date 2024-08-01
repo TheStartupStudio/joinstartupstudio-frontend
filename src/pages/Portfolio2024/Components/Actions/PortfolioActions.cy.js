cd08366d0fd48a593c9e978d2c2f78b896e463d0
Ardit Qerimi <arditqerimi88@gmail.com>
2024-08-01 16:22:14 +0200

    last changes

--- src/pages/Portfolio2024/Components/Actions/PortfolioActions.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PortfolioActions from './PortfolioActions'
-
-describe('<PortfolioActions />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<PortfolioActions />)
-  })
-})
