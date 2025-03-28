b8a1d79ed908367296c885c37fdbd9dd101b62ab
Ardi Osmani <ardiosmani02@gmail.com>
2025-03-28 11:12:54 +0100

    feat(experience): integrate Redux for dynamic experience data rendering

--- src/components/AcademyPortfolio/ExperiencePortfolio.js
+++ src/components/AcademyPortfolio/ExperiencePortfolio.js
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import nemoursHospital from '../../assets/images/academy-icons/nemours-hospital.png'
+import { useState, useEffect } from 'react'
 import experienceIcon from '../../assets/images/academy-icons/svg/experience.svg'
 import universityFlorida from '../../assets/images/academy-icons/universirty-florida.png'
 import EditExperience from './EditExperience'
 import PortfolioContent from './PortfolioContent'
 import PortfolioWrapper from './PortfolioWrapper'
 import NewExperience from './NewExperience'
+import { useDispatch, useSelector } from 'react-redux'
+import { getMyWorkExperiences } from '../../redux/portfolio/Actions'
 
 function ExperiencePortfolio() {
   const [isOpen, setIsOpen] = useState(false)
   const [openNew, setOpenNew] = useState(false)
+  const [selectedExperience, setSelectedExperience] = useState(null)
+
+  const experienceData = useSelector(
+    (state) => state.portfolio.howSection?.myProductivity?.workExperiences?.data
+  )
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getMyWorkExperiences())
+  }, [dispatch])
+
+  const handleEdit = (experience) => {
+    setSelectedExperience(experience)
+    setIsOpen(true)
+  }
 
   return (
     <>
@@ -18,36 +34,29 @@ function ExperiencePortfolio() {
         title={'Experience'}
         setOpenNew={setOpenNew}
       >
-        <PortfolioContent
-          setIsOpen={setIsOpen}
-          imgSrc={universityFlorida}
-          title={'Social Media Manager'}
-          institution={'Tri Delta Phi Sorority at UFC'}
-          duration={'September 2020 - May 2024'}
-          link={'https://instagram.com/tri-delta-phi-fsu'}
-          fullText={`Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-      quae ab illo inventore veritatis et quasi architecto beatae vitae
-      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
-      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
-      qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...`}
-        />
-        <PortfolioContent
-          setIsOpen={setIsOpen}
-          imgSrc={nemoursHospital}
-          title={'Marketing Intern'}
-          institution={"Nemours Children's Hospital"}
-          duration={'May 2021 - August 2021'}
-          link={'https://nemours.org/childrens-healthcare'}
-          fullText={`Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-      quae ab illo inventore veritatis et quasi architecto beatae vitae
-      dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
-      sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
-      qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...`}
-        />
+        {experienceData.map((experience) => (
+          <PortfolioContent
+            key={experience.id}
+            setIsOpen={() => handleEdit(experience)}
+            imgSrc={experience.imageUrl || universityFlorida}
+            title={experience.organizationName || null}
+            institution={experience.location || null}
+            duration={experience.startDate ? 
+              `${new Date(experience.startDate).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })} - ${
+                experience.currentPosition ? 'Present' : 
+                new Date(experience.endDate).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })
+              }` : null
+            }
+            link={experience.website || null}
+            fullText={experience.description?.replace(/<[^>]*>/g, '') || null}
+          />
+        ))}
       </PortfolioWrapper>
-      <EditExperience isOpen={isOpen} setIsOpen={setIsOpen} />
+      <EditExperience 
+        isOpen={isOpen} 
+        setIsOpen={setIsOpen}
+        experienceData={selectedExperience}
+      />
       <NewExperience isOpen={openNew} setIsOpen={setOpenNew} />
     </>
   )
